0290eca6fdfbd3f771231311e22b7d8f2130faf3
Leon Malisov <leonmalisov@gmail.com>
2020-01-13 17:29:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ def key_for_min_value(name_hash)
   smallest_value = nil
   cheapest_item = nil
   name_hash.each do |item, value|
-    if smallest_value == "" || value < smallest_value
+    if smallest_value == nil || value < smallest_value
       smallest_value = value
       cheapest_item  = item
     end
